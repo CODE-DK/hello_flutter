be8279470483b14add7bce4ab7332ee8ae3e4cdb
DK <dmitry.komovsky@raiffeisen.ru>
2022-01-06 10:55:36 +0500

    Add first stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Some appbar title'),
-      centerTitle: true,
-      backgroundColor: Colors.pink,
-    ),
-    backgroundColor: Colors.black,
-    body: const Center(
-        child: Text('Hello flutter',
-            style: TextStyle(
-                fontSize: 30, 
-                fontWeight: FontWeight.bold, 
-                color: Colors.white
-                ),
-            textDirection: TextDirection.ltr)
+  runApp(const MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            floatingActionButton: const FloatingActionButton(
+              onPressed: null,
+              child: Icon(Icons.cloud_download),
             ),
-  )));
+            appBar: AppBar(
+              title: const Text('My first app'),
+              centerTitle: true,
+            ),
+            backgroundColor: Colors.indigo,
+            body: Center(
+                child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        LinearProgressIndicator(value: 23),
+                        Text('23 %',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                        Text('Press button to download',
+                            style: TextStyle(color: Colors.white, fontSize: 20))
+                      ],
+                    )))));
+  }
 }
